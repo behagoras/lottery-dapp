f7356ed4f5d7d0185c8c66b5741993e8e139962a
David Behar Lombrozo <david.behar@improving.com>
2021-06-20 13:58:21 -0500

    fix contract 4.17

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,51 +1,39 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
-    address payable [] public players;
-    // Recomendacion usar tipos mapping para manejar arrays
-    // mapping(Key type => Valuetype) public name; 
-    // Basicamente es una lista que almacena arrays ([1,0x0001], [1,0x0001]) 
-    // https://docs.soliditylang.org/en/v0.8.5/types.html#mapping-types
+    address public winner;
+    address[] public players;
 
-    // Este es el constructor del contrato en versiones de solidity 5.0 para arriba se define como tal 
-    // ya que no se permite que una funcion tenga el mismo nombre que la del contrato y se borra el 
-    // keyword function
-    constructor() public {
+    function Lottery() public {
         manager = msg.sender;
     }
-    
     function enter() public payable {
         require(msg.value > .01 ether);
-        //Aqui lo que pasa es que msg.sender es de tipo address, por lo que primero hay que
-        //usar un tipo para parsearlo
-        players.push(payable(msg.sender));     
+        players.push(msg.sender);
     }
-
     function random() private view returns (uint) {
-        //https://ethereum.stackexchange.com/questions/63121/version-compatibility-issues-in-solidity-0-5-0-and-0-4-0/63128
-        //esto del abi enconded es por que de la verision 4 a 5 de solidity se rompe la funcion keccak256 y hay que
-        //pasarle los argumentos codificados
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+        return uint(keccak256(block.difficulty, now, players));
     }
-    
-    //Asi mismo las funciones que involucran transaccion
-    function pickWinner() public payable restricted {
+    function pickWinnerWithCommissions() public restricted {
         uint index = random() % players.length;
-        //Todos los tipos address que esten involucrados en cualquier tipo de transaccion
-        //Necesitan incluir el keyword payable para poder utilizar los metodos de send o transfer
-        players[index].transfer(address(this).balance);
-        players = new address payable[](0);
+        winner = players[index];
+        uint commission = this.balance * 1 / 10;
+        uint winnerAmount = this.balance * 9 / 10;
+        players[index].transfer(winnerAmount);
+        winner.transfer(winnerAmount);
+        players = new address[](0);
     }
-    
+    function pickWinner() public restricted {
+        uint index = random() % players.length;
+        players[index].transfer(this.balance);
+        players = new address[](0);
+    }
     modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    //En versiones de solidity >=5.0 es necesario decirle a las funciones de tipo vista
-    //que el return solo es utilizado dentro de la ejecucion del contrato con el keyword memory
-    function getPlayers() public view returns (address payable[] memory) {
+    function getPlayers() public view returns (address[]) {
         return players;
     }
-}   
+}
