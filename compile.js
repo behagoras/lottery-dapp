77a7a050682e2c4cafcb96d4e5e8ff29ae8fae5b
TEGDV <apjair97@gmail.com>
2021-06-21 11:20:06 -0500

    :wrench: Refactor to handle contracts verson above ^0.8.0

--- compile.js
+++ compile.js
@@ -5,4 +5,23 @@ const solc = require("solc");
 const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
 const source = fs.readFileSync(lotteryPath, "utf8");
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+// Cambios necesarios para manejar versiones por arriba de sol 5
+// Recordar actualizar     "solc": "^0.4.17" => ^0.8.5 si se quire usar la version actualizada del contrato,
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'Lottery8.sol' : {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+};
+
+module.exports = solc.compile(JSON.stringify(input));
